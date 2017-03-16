8cd403fc94e545c94eedcf71b907c8f8c05b531b
Erik Jacobsen <slowerthanyou@gmail.com>
2017-03-16 09:25:11 -0700

    broke it this morning, had to fix it

--- main2.js
+++ main2.js
@@ -5,32 +5,25 @@ setInterval (function(clockStart){
   var hh = newDate.getHours()
   var m = newDate.getMinutes()
   var s = newDate.getSeconds()
-  var h = hh
-
   if(hh >= 12) {
 
-      h = hh - 12
-
+      hh = hh - 12
       dd = "PM"
     } 
 
-    if (hh == 0) {
-      hh = 12
-    }
+  hh = hh < 10 ? "0" + hh : hh
+  m = newDate.getMinutes() < 10 ? "0" + newDate.getMinutes() : newDate.getMinutes()
+  s = newDate.getSeconds() < 10 ? "0" + newDate.getSeconds() : newDate.getSeconds()
 
-  h = h < 10 ? "0" + h : h
-  m = m < 10 ? "0" + m: m
-  s = s < 10 ? "0" + s : s
-
-  var correction = h +":"+ m +":" + s + " " + dd
-  var backColor = "#" + h.toString(16) + m.toString(16) + s.toString(16)
-  var opposite = "#" + s.toString(16) + m.toString(16) + h.toString(16) 
-
+  var correction = hh +":"+ m +":" + s + " " + dd
+  var backColor = "#" + hh.toString(16) + m.toString(16) + s.toString(16)
+  var opposite = "#" + s.toString(16) + m.toString(16) + hh.toString(16) 
+console.log(backColor)
   $("#finalCountdown").html(`${correction}`)
   $("#finalCountdown").css('background-color', backColor)
   $("body").css('background-color', opposite)
   $("#finalCountdown").css("color", opposite)
- console.log(opposite)
+ 
 }, 1000)
 
 
