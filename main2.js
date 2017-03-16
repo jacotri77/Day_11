44c6ec961cb45977a1440e637db078f7898f691a
Erik Jacobsen <slowerthanyou@gmail.com>
2017-03-15 19:53:15 -0700

    added a new font

--- main2.js
+++ main2.js
@@ -18,18 +18,18 @@ setInterval (function(clockStart){
       hh = 12
     }
 
-
-
   h = h < 10 ? "0" + h : h
   m = m < 10 ? "0" + m: m
   s = s < 10 ? "0" + s : s
 
   var correction = h +":"+ m +":" + s + " " + dd
   var backColor = "#" + h.toString(16) + m.toString(16) + s.toString(16)
+  var opposite = "#" + s.toString(16) + m.toString(16) + h.toString(16) 
 
   $("#finalCountdown").html(`${correction}`)
-  $("body").css('background-color', backColor)
- 
+  $("#finalCountdown").css('background-color', backColor)
+  $("body").css('background-color', opposite)
+ console.log(opposite)
 }, 1000)
 
 
